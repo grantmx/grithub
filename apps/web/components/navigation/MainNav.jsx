04117a792bb0314efe7d8b7e00155e6025212f09
grantmx <grantmx@users.noreply.github.com>
2023-02-05 05:46:20 -0500

    Update MainNav.jsx

--- apps/web/components/navigation/MainNav.jsx
+++ apps/web/components/navigation/MainNav.jsx
@@ -17,22 +17,20 @@ function MainNav({ page }){
                     />
                 </a>
                 <div className={home.navbarNav}>
-                    <Link href="/programs" className="nav-link fs-5 me-3 text-dark">
+                    <Link href="/programs" className="nav-link fs-5 me-3 me-lg-5 text-dark">
                         <Image src="/assets/people.svg" width={20} height={20} className="d-lg-none" alt="Program Events at GRITHub" />Training
                     </Link>
 
-                    <Link href="/consulting" className="nav-link fs-5 me-3 text-dark">
+                    <Link href="/consulting" className="nav-link fs-5 me-3 me-lg-5 text-dark">
                         <Image src="/assets/event_seat.svg" width={20} height={20} className="d-lg-none" alt="Program Events at GRITHub" />Consulting
                     </Link>
 
-                    <a className="nav-link fs-5 me-3 text-dark" href="/#incubator">
+                    <a className="nav-link fs-5 me-3 me-lg-5 text-dark" href="/#incubator">
                         <Image src="/assets/sun.svg" width={20} height={20} className="d-lg-none" alt="Business Incubation" />
                         Incubation
                     </a>
 
-                    
-
-                    <a className="nav-link fs-5 me-3 text-dark" href="/#coworking">
+                    <a className="nav-link fs-5 me-3 me-lg-5 text-dark" href="/#coworking">
                         <Image src="/assets/activity.svg" width={20} height={20} className="d-lg-none" alt="GRITHub Coworking" />
                         CoWork
                     </a>
