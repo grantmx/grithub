c27082c1178edeed388b98ab373e46c928b0b0d6
grantmx <grantmx@users.noreply.github.com>
2023-07-26 04:06:11 -0400

    Update loadshedding.service.js

--- apps/kiosk/src/services/loadshedding.service.js
+++ apps/kiosk/src/services/loadshedding.service.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
-import { eskomSePush_key } from '../lib/constants'
-
 
 export async function getLoadSheddingEvents({ id }){
     return axios({
         method: "GET",
         baseURL: "https://developer.sepush.co.za/business/2.0/area",
         headers: {
-            token: eskomSePush_key
+            token: process.env.eskomSePush_key
         },
         params: {
             id,
