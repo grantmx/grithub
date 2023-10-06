796db6e3b166bcb557a97630885a57bf8a301984
grantmx <grantmx@users.noreply.github.com>
2023-10-06 08:54:15 -0400

    firebase revert

--- apps/captivate/components/Handheld/ButtonRow.jsx
+++ apps/captivate/components/Handheld/ButtonRow.jsx
@@ -29,7 +29,6 @@ function ButtonRow({ back, next, current = 0 }){
             )}
             
 
-
             <Link  href={{ href: "handheld", query: { goto: 1 }}} role="button" className={Style.transButton}>
                 <svg id="icon-home" viewBox="0 0 24 24" width="40" height="40" fill="#fff">
                     <path d="M9.984 20.016h-4.969v-8.016h-3l9.984-9 9.984 9h-3v8.016h-4.969v-6h-4.031v6z"></path>
