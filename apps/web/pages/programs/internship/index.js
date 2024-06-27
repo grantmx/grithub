dc33d59e1421a9c1a7b371b01eac2b3fa79fc13e
grantmx <grantmx@users.noreply.github.com>
2024-06-27 01:43:35 -0400

    events

--- apps/web/pages/programs/internship/index.js
+++ apps/web/pages/programs/internship/index.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import intern from "public/assets/coworkers-working-office.jpg"
 import Style from "./internship.module.scss"
 import clsx from "clsx";
-import Link from "next/link"
 
 
 
