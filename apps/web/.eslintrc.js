2164a2a66f9b32946be42b118975c7ab8c3ad011
grantmx <grantmx@users.noreply.github.com>
2024-12-17 04:29:37 -0500

    Update .eslintrc.js

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,25 @@
 module.exports = {
 	root: true,
-	extends: ["next/babel","next/core-web-vitals"],
-	rules: { 
-		"react/no-unescaped-entities": ["error", {"forbid": [">", "}"]}]
+	extends: ["next/babel", "next/core-web-vitals"],
+	rules: {
+		"react/no-unescaped-entities": ["error", { "forbid": [">", "}"] }],
+		"no-undef": "error",
+		"no-unused-vars": "warn",
+		"no-unused-expressions": "warn",
+		"no-extra-semi": "warn"
+	},
+	"overrides": [
+		{
+			"files": ["*.js", "*.mjs"],
+			// This is the default parser of ESLint
+			"parser": "espree",
+			"parserOptions": {
+				"ecmaVersion": "latest"
+			}
+		}
+	],
+	"env": {
+		"browser": true,
+		"es6": true
 	}
 };
