5d7c284d7ff61e8e71db793f7033f6e3f25d56dc
grantmx <grantmx@users.noreply.github.com>
2025-04-15 05:33:35 -0400

    build fix

--- apps/web/app/(pages)/programs/incubation/competition/success/page.js
+++ apps/web/app/(pages)/programs/incubation/competition/success/page.js
@@ -1,43 +1,16 @@
-"use client"
-
-import axios from "axios"
 import Link from "next/link";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-// import { celebrationTime } from "lib/utils/celebrationTime";
+import Success from "./Success";
+import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic';
 
-
-function InternshipSuccess(){
-    const searchParams = useSearchParams()
-    const firstName = searchParams.get('first_name')
-    const email = searchParams.get('email')
-
-    // useEffect(() => {
-	// 	celebrationTime()
-
-	// }, [])
-
-    useEffect(() => {
-        if( email && firstName ){
-            
-            axios.post("/api/email/internship", {
-                subject: `Thank you for your Innovative Ideas Competition submission, ${firstName}! 🎉`,
-                to: email,
-                heading: `Hey, ${firstName}! 👋`,
-                message: `
-                    <p>Your application submission to the Innovative Ideas Competition was successful.  Thanks!</p>
-                    <p>If you are a finalist, we will be in contact with you regarding follow-up and additional information. All the best, and thank you again for your interest.</p>
-                    <p>- Marshall</p>
-                `
-            })    
-        }
-
-    }, [ email, firstName ])
-
-
+async function InternshipSuccess(){
     return(
         <>
+            <Suspense>
+                <Success />
+            </Suspense>
+
             <section className="container-xxl d-flex p-md-5 p-4 flex-column mb-5" id="coworking">
                 <div className="col-12 d-flex flex-column">
                     <span className="display-1 text-center">🥳</span>
