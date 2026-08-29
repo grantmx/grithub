5bc7a82fe52ee7d4f0d558c97644937627c47d65
grantmx <grantmx@users.noreply.github.com>
2026-08-29 03:56:26 -0400

    Update page.js

--- apps/web/app/(home)/page.js
+++ apps/web/app/(home)/page.js
@@ -119,7 +119,7 @@ export default async function Home(){
 										<Image 
 											src={rotary}
 											alt="Rotary Club of George" 
-											className={clsx(Style.whiteLogo)}
+											className={clsx(Style.partnerLogo)}
 											priority={true}
 											fetchPriority='high'
 										/>
